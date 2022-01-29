b6dd30a1b51c5bb40c1c1d748d21a8e0b223c397
user <user@example.com>
2022-01-29 22:26:07 +0900

    Added submodule installation process

--- .install_dotfiles.sh
+++ .install_dotfiles.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 git ls-files -z | rsync --files-from=- -abv0  . ~
+git submodule status |
+column -t |
+tr -s ' ' |
+cut -d ' ' -f 2 |
+while read dir
+    do rsync -av --delete "${dir}/" "${HOME}/${dir}"
+done
